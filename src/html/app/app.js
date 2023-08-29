56458166e612936f1b2a7b29329dff8ebf5b3fa5
TheRealGabHas <69684024+TheRealGabHas@users.noreply.github.com>
2023-08-29 15:23:41 +0200

    Added MC-Pedia and responsiveness update

--- src/html/app/app.js
+++ src/html/app/app.js
@@ -1,10 +1,2 @@
-const func = (x, y) => {
-  return x + y;
-}
-
-function f(x, y) {
-    return x + y;
-}
-
-console.log(func(1, 2));
-console.log(f(1, 2));
+const app = () => { return "Nothing to see here..."; }
+app();
