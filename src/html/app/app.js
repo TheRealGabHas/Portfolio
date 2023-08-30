4bdb3279e0cf1ff29a2f32be9e11a963c401e419
TheRealGabHas <69684024+TheRealGabHas@users.noreply.github.com>
2023-08-30 10:30:30 +0200

    Removed useless EventListener

--- src/html/app/app.js
+++ src/html/app/app.js
@@ -1,9 +1,7 @@
-document.addEventListener('click', () => {
-    let dialog = document.getElementById('credits-dialog');
-    document.getElementById('show').onclick = () => {
-        dialog.show();
-    };
-    document.getElementById('hide').onclick = () => {
-        dialog.close();
-    };
-})
+let dialog = document.getElementById('credits-dialog');
+document.getElementById('show').onclick = () => {
+    dialog.show();
+};
+document.getElementById('hide').onclick = () => {
+    dialog.close();
+};
