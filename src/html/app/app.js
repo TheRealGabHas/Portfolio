4f507ca3b8cb35364574c79c45e5aa702346efbf
TheRealGabHas <69684024+TheRealGabHas@users.noreply.github.com>
2023-09-03 13:38:14 +0200

    Minor changes and scroll on credits display

--- src/html/app/app.js
+++ src/html/app/app.js
@@ -1,6 +1,7 @@
 let dialog = document.getElementById('credits-dialog');
 document.getElementById('show').onclick = () => {
     dialog.show();
+    window.scrollTo(0, document.body.scrollHeight);
 };
 document.getElementById('hide').onclick = () => {
     dialog.close();
